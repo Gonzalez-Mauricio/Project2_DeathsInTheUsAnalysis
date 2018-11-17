8d8b647823eeafd19fb5ea6a578c0e2efb6a6a3b
David Marcus <41026498+dmarcus9@users.noreply.github.com>
2018-11-16 18:33:20 -0600

    Add files via upload

--- DeathPieChart.js
+++ DeathPieChart.js
@@ -1,41 +1,31 @@
-// var trace1 = {
-//   labels: ["Heart Disease", "Malignant Neoplasms", "Unintentional Injury", "Chronic Low. Respiratory Disease",
-//       "Cerebrovascular", "Alzheimer's Disease", "Diabetes Mellitus", "Influenza & Pneumonia"
-//       "Nephritis", "Suicide", "Septicemia", "Liver Disease"
-//       "Hypertension", "Parkinson's Disease", "Pneumonitis", "All Others"],
-//   values: [635260, 598038, 161374, 154596, 142142, 116103, 80058, 51537,
-//           50046, 44965, 40613, 40545, 33246, 29697, 19715, 546313],
-//   type: 'pie'
-// };
-//
-// var data = [trace1];
-//
-// var layout = {
-//   title: "15 Leading Causes of Deaths, United States, 2016",
-// };
-//
-// Plotly.newPlot("plot", data, layout);
 
 console.log("in js");
 
+
 var data = [{
   values: [635260, 598038, 161374, 154596, 142142, 116103, 80058, 51537,
-          50046, 44965, 40613, 40545, 33246, 29697, 19715, 546313],
-  labels: ["Heart Disease", "Malignant Neoplasms", "Unintentional Injury", "Chronic Low. Respiratory Disease",
+          50046, 44965, 40613, 40545, 33246, 29697, 19715, 19362, 526951],
+  // For legend...
+  // labels: ["Heart Disease", "Malignant Neoplasms", "Unintentional Injury", "Chronic Low. Respiratory Disease",
+  //           "Cerebrovascular", "Alzheimer's Disease", "Diabetes Mellitus", "Influenza & Pneumonia",
+  //           "Nephritis", "Suicide", "Septicemia", "Liver Disease",
+  //           "Hypertension", "Parkinson's Disease", "Pneumonitis", "Homicide", "All Others"],
+  text: ["Heart Disease", "Malignant Neoplasms (Cancer)", "Unintentional Injury", "Chronic Low. Respiratory Disease",
             "Cerebrovascular", "Alzheimer's Disease", "Diabetes Mellitus", "Influenza & Pneumonia",
             "Nephritis", "Suicide", "Septicemia", "Liver Disease",
-            "Hypertension", "Parkinson's Disease", "Pneumonitis", "All Others"],
-  text: ["Heart Disease", "Malignant Neoplasms", "Unintentional Injury", "Chronic Low. Respiratory Disease",
-            "Cerebrovascular", "Alzheimer's Disease", "Diabetes Mellitus", "Influenza & Pneumonia",
-            "Nephritis", "Suicide", "Septicemia", "Liver Disease",
-            "Hypertension", "Parkinson's Disease", "Pneumonitis", "All Others"],
+            "Hypertension", "Parkinson's Disease", "Pneumonitis", "Homicide", "All Others"],
   type: 'pie'
 }];
 
+// TODO: adjust center accompanying text
+// TODO: rotate pie so largest slices are on top
+// TODO: Unintentional injury details...as a separate graph
+// TODO: add text boxes? with phobias highlighted
+
 var layout = {showlegend: false,
-  title: "15 Leading Causes of Deaths, United States, 2016",
+  title: "Here is a breakdown of the top 16 causes, which accounts for almost 81% of total.",
   height: 800,
-  width: 1200
-};
+  width: 1200,
+  };
 
 Plotly.newPlot('plot', data, layout);
